d11cf7c73dd030647ad94541f0fecd89f60b8be5
3xpl0itz <37809476+3xpl0itz@users.noreply.github.com>
2021-08-05 18:36:24 -0700

    Import cleanup

--- src/main/java/com/expl0itz/worldwidechat/WorldwideChat.java
+++ src/main/java/com/expl0itz/worldwidechat/WorldwideChat.java
@@ -21,7 +21,6 @@ import org.bukkit.plugin.PluginDescriptionFile;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.plugin.java.JavaPluginLoader;
 import org.bukkit.scheduler.BukkitRunnable;
-import org.bukkit.scheduler.BukkitTask;
 
 import com.expl0itz.worldwidechat.commands.WWCConfiguration;
 import com.expl0itz.worldwidechat.commands.WWCGlobal;
--- src/main/java/com/expl0itz/worldwidechat/inventory/configuration/ConfigurationWatsonSettingsGUI.java
+++ src/main/java/com/expl0itz/worldwidechat/inventory/configuration/ConfigurationWatsonSettingsGUI.java
@@ -2,7 +2,6 @@ package com.expl0itz.worldwidechat.inventory.configuration;
 
 import java.io.IOException;
 
-import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.Material;
 import org.bukkit.conversations.ConversationFactory;
