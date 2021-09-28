b53c309d10c5b2d0129636416c163a75b5cd9920
3xpl0itz <37809476+3xpl0itz@users.noreply.github.com>
2021-09-27 23:01:44 -0700

    Added some personal testing scripts, re-added DeluxeChatListener, updated pom to include a stripped version of DeluxeChat
    NOT PIRACY. This jar only contains the TWO classes I need to compile my project. Everything else is stripped out. These classes are also just listeners, meaning they don't contain any actual valuable code to the plugin. Since the developers have no official API or any kind of maven dependency to source, this is one of the only ways without using reflection (which I'd rather avoid.)

---
+++ dependencies/copy_worldwidechat_jar.bat
@@ -0,0 +1,8 @@
+@echo off
+copy /Y "C:\Users\Dominic\Desktop\WorldwideChat\WorldwideChat\target\WorldwideChat.jar" "C:\Users\Dominic\Desktop\MC Servers\personal_wwc_test_server\plugins"
+copy /Y "C:\Users\Dominic\Desktop\WorldwideChat\WorldwideChat\target\WorldwideChat.jar" "C:\Users\Dominic\Desktop\MC Servers\personal_wwc_test_server_188\plugins"
+copy /Y "C:\Users\Dominic\Desktop\WorldwideChat\WorldwideChat\target\WorldwideChat.jar" "C:\Users\Dominic\Desktop\MC Servers\personal_wwc_test_server_194\plugins"
+copy /Y "C:\Users\Dominic\Desktop\WorldwideChat\WorldwideChat\target\WorldwideChat.jar" "C:\Users\Dominic\Desktop\MC Servers\personal_wwc_test_server_1102\plugins"
+copy /Y "C:\Users\Dominic\Desktop\WorldwideChat\WorldwideChat\target\WorldwideChat.jar" "C:\Users\Dominic\Desktop\MC Servers\personal_wwc_test_server_1112\plugins"
+copy /Y "C:\Users\Dominic\Desktop\WorldwideChat\WorldwideChat\target\WorldwideChat.jar" "C:\Users\Dominic\Desktop\MC Servers\personal_wwc_test_server_1122\plugins"
+copy /Y "C:\Users\Dominic\Desktop\WorldwideChat\WorldwideChat\target\WorldwideChat.jar" "C:\Users\Dominic\Desktop\MC Servers\personal_wwc_test_server_1132\plugins"
--- src/main/java/com/expl0itz/worldwidechat/listeners/DeluxeChatListener.java
+++ src/main/java/com/expl0itz/worldwidechat/listeners/DeluxeChatListener.java
@@ -1,7 +1,32 @@
 package com.expl0itz.worldwidechat.listeners;
 
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
+
+import com.expl0itz.worldwidechat.WorldwideChat;
+import com.expl0itz.worldwidechat.util.CommonDefinitions;
+import com.expl0itz.worldwidechat.util.PlayerRecord;
+
+import me.clip.deluxechat.events.DeluxeChatEvent;
 
 public class DeluxeChatListener implements Listener {
 
+    private WorldwideChat main = WorldwideChat.getInstance();
+    
+    @EventHandler(priority = EventPriority.HIGHEST)
+    public void onDeluxeChat(DeluxeChatEvent event) { 
+        if (!main.getActiveTranslator(event.getPlayer().getUniqueId().toString()).getUUID().equals("") || !main.getActiveTranslator("GLOBAL-TRANSLATE-ENABLED").getUUID().equals("")) {
+            String out = CommonDefinitions.translateText(event.getChatMessage(), event.getPlayer());
+            
+            //Normally, since DeluxeChat has its own weird chat thread, this method is called twice,
+            //and a player is given two attempted/successful translations. Let's subtract one.
+            PlayerRecord currRecord = main.getPlayerRecord(event.getPlayer().getUniqueId().toString(), true);
+            currRecord.setAttemptedTranslations(currRecord.getAttemptedTranslations()-1);
+            currRecord.setSuccessfulTranslations(currRecord.getSuccessfulTranslations()-1);
+            
+            event.setChatMessage(out);
+        }
+    }
+	
 }
