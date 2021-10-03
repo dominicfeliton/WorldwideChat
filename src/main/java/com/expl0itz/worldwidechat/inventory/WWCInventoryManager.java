244b55f68bef1253aa9f9cf263d805a7d80d922d
3xpl0itz <37809476+3xpl0itz@users.noreply.github.com>
2021-10-03 00:04:47 -0700

    Remove unused import

--- src/main/java/com/expl0itz/worldwidechat/inventory/WWCInventoryManager.java
+++ src/main/java/com/expl0itz/worldwidechat/inventory/WWCInventoryManager.java
@@ -4,7 +4,6 @@ import java.util.UUID;
 
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
-import org.bukkit.Material;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
